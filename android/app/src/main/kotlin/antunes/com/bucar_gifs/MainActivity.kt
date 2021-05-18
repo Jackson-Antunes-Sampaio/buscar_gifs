d05bf0ed1cd23937f11fe54a3a5c86d43a5e4fec
Jackson Antunes <jackson.antunes.sampaio@gmail.com>
2021-05-18 11:19:47 -0300

    first commit

---
+++ android/app/src/main/kotlin/antunes/com/bucar_gifs/MainActivity.kt
@@ -0,0 +1,6 @@
+package antunes.com.bucar_gifs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+
+
+
+import 'package:bucar_gifs/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:bucar_gifs/ui/home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
